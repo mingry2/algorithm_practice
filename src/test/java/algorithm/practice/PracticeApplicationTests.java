dc6c7baab34ab10468c9e91faba5fa22f9c13805
minkyoung kim <minry20415@naver.com>
2023-03-25 19:49:44 +0900

    BOJ 알고리즘 연습

---
+++ src/test/java/algorithm/practice/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package algorithm.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

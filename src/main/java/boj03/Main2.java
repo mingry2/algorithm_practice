357199d50847edf525507be18fa57787966a207d
minkyoung kim <minry20415@naver.com>
2023-03-27 22:30:32 +0900

    boj03 - [8393] 합

---
+++ src/main/java/boj03/Main2.java
@@ -0,0 +1,18 @@
+package boj03;
+
+import java.util.Scanner;
+
+//[8393] 합
+public class Main2 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+
+		int ans = 0;
+		for (int i = 0; i <=N; i++) {
+			ans += i;
+		}
+		System.out.println(ans);
+	}
+
+}

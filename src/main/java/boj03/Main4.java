362677c0bb0ffe4285d8f8a2bef0a3a54e918d24
minkyoung kim <minry20415@naver.com>
2023-03-27 23:09:41 +0900

    [25314] 코딩은 체육과목 입니다

---
+++ src/main/java/boj03/Main4.java
@@ -0,0 +1,20 @@
+package boj03;
+
+import java.util.Scanner;
+
+//[25314] 코딩은 체육과목 입니다
+public class Main4 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt() / 4;
+
+//		System.out.println("long ".repeat(N) + "int");
+
+		String str[] = new String[N];
+		for (int i = 0; i < N; i++) {
+			System.out.print("long ");
+		}
+		System.out.println("int");
+	}
+
+}

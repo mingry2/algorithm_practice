760d44735afa38e79bb6001214b221ba616c25ef
minkyoung kim <minry20415@naver.com>
2023-03-28 21:51:08 +0900

    [11022] A+B -8

---
+++ src/main/java/boj03/Main7.java
@@ -0,0 +1,32 @@
+package boj03;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+//[11022] A+B -8
+public class Main7 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int T = Integer.parseInt(br.readLine());
+
+		StringTokenizer st;
+		int A;
+		int B;
+
+		for (int i = 1; i <= T; i++) {
+			st = new StringTokenizer(br.readLine());
+			A = Integer.parseInt(st.nextToken());
+			B = Integer.parseInt(st.nextToken());
+			bw.write("Case #" + i + ": " +
+					A + " + " + B + " = " +
+					(A + B) + "\n");
+		}
+		bw.close();
+	}
+
+}

3803f57d1b354191d7419937e7d5759bba55d6bd
minkyoung kim <minry20415@naver.com>
2023-03-28 22:48:57 +0900

    [2439] 별 찍기 - 2

---
+++ src/main/java/boj03/Main9.java
@@ -0,0 +1,23 @@
+package boj03;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main9 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		br.close();
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n - i; j++) {
+				System.out.print(" ");
+			}
+			for (int k = 1; k <= i; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}

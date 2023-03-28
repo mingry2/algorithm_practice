1c218087d3ea80419dfc10713861f8b59e523807
minkyoung kim <minry20415@naver.com>
2023-03-28 21:03:55 +0900

    [15552] 빠른 A+B

---
+++ src/main/java/boj03/Main5.java
@@ -0,0 +1,27 @@
+package boj03;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+//[15552] 빠른 A+B
+public class Main5 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st; //공백을 기준으로 입력받을 때 사용
+
+		int T = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < T; i ++) {
+			st = new StringTokenizer(br.readLine());
+			bw.write((Integer.parseInt(st.nextToken()) + (Integer.parseInt(st.nextToken())) + "\n"));
+		}
+		bw.close();
+	}
+
+}

41672caf582fa9cf0e2becd7f0107916f84c3850
minkyoung kim <minry20415@naver.com>
2023-03-28 23:25:04 +0900

    [10951] A+B -4

---
+++ src/main/java/boj03/Main11.java
@@ -0,0 +1,29 @@
+package boj03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+//[10951] A+B - 4
+public class Main11 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st;
+		int a;
+		int b;
+		String str;
+
+		while((str = br.readLine()) != null) {
+			st = new StringTokenizer(str, " ");
+			a = Integer.parseInt(st.nextToken());
+			b = Integer.parseInt(st.nextToken());
+
+			System.out.println(a+b);
+		}
+
+	}
+
+}

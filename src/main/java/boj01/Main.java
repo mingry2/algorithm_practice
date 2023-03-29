129d9abbfd9da52625e109afe7d7770b48c6aa31
minkyoung kim <minry20415@naver.com>
2023-03-29 21:57:55 +0900

    [10869] 사칙연산

---
+++ src/main/java/boj01/Main.java
@@ -0,0 +1,26 @@
+package boj01;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+//[10869] 사칙연산
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+
+		br.close();
+
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+	}
+
+}

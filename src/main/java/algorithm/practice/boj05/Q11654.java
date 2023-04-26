220cbbe677843ec075d706c79332043c88fae060
minkyoung kim <minry20415@naver.com>
2023-04-26 22:51:54 +0900

    [11654] 아스키 코드

---
+++ src/main/java/algorithm/practice/boj05/Q11654.java
@@ -0,0 +1,15 @@
+package algorithm.practice.boj05;
+
+import java.io.IOException;
+
+//아스키 코드
+public class Q11654 {
+	public static void main(String[] args) throws IOException {
+
+		int result = System.in.read();
+
+		System.out.print(result);
+
+	}
+
+}
